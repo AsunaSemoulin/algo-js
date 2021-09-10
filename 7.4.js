92c0bc1826bcdf977fb477032f727d11912caf2c
AsunaSemoulin <89163001+AsunaSemoulin@users.noreply.github.com>
2021-09-10 12:24:49 +0200

    Update 7.4.js

--- 7.4.js
+++ 7.4.js
@@ -18,40 +18,52 @@ let list = [];
 console.log(board);
 let actionNumber;
 
-fs.readFile('list.txt', 'utf8' , (err, data) => {
-    if (err) {
-      console.error(err)
-      return
-    }
-    let dataString = data.toString();
-    let finalData = dataString.split(",");
-    list = finalData;
-})
+let saveFile = () => {
+    let object = new Object();
+    object.save = list;
+
+    fs.writeFile('list.json', JSON.stringify(object), err => {
+        if (err) {
+            console.error(err);
+            return;
+        }
+    })
+}
+
+let openFile = () => {
+    fs.readFile('list.json', 'utf8' , (err, data) => {
+        if (err) {
+          console.error(err)
+          return
+        }
+        let dataString = data["save"].toString();
+        let finalData = dataString.split(",");
+        list = finalData;
+    })
+}
 
 while (actionNumber != 4) {
+
     let actionNumber = readlineSync.question("Enter your action's number: ");
 
     if (actionNumber == 1) {
+        openFile();
         console.log(list);
     }
     else if (actionNumber == 2) {
         let newPizzaFlavor = readlineSync.question("Pizza flavor: ");
 
         list.push(newPizzaFlavor);
+        saveFile();
     }
     else if (actionNumber == 3) {
         let removePizzaFlavor = readlineSync.question("Pizza flavor: ");
 
         let index = list.indexOf(removePizzaFlavor);
         list.splice(index, 1);
+        saveFile();
     }
     else {
-        fs.writeFile('list.txt', list.toString(), err => {
-            if (err) {
-                console.error(err);
-                return;
-            }
-        })
         return;
     }
 }
